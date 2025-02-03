8dee04a6b9a81548f56d19e071e3053fb79af88b
nikita-plotnytskyi <nikita.plotnitckiy@gmail.com>
2025-02-03 09:36:35 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,24 +2,99 @@ from abc import ABC
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount: int, max_amount: int) -> None:
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(
+        self,
+        owner: (
+            "ChildrenSlideLimitationValidator",
+            "AdultSlideLimitationValidator"
+        ),
+        name: str
+    ) -> None:
+        self.name = "_" + name
+
+    def __get__(
+        self,
+        instance: None,
+        owner: None
+    ) -> None:
+        pass
+
+    def __set__(
+        self,
+        instance: (
+            "ChildrenSlideLimitationValidator",
+            "AdultSlideLimitationValidator"
+        ),
+        value: int
+    ) -> None:
+        if not isinstance(value, int):
+            raise TypeError("Тип значення маэ бути int")
+        if self.min_amount <= value <= self.max_amount:
+            setattr(instance, self.name, value)
 
 
 class Visitor:
-   pass
+    def __init__(self, name: str, age: int, weight: int, height: int) -> None:
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class SlideLimitationValidator(ABC):
-    pass
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(min_amount=4, max_amount=14)
+    height = IntegerRange(min_amount=80, max_amount=120)
+    weight = IntegerRange(min_amount=20, max_amount=50)
+
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        self.age = age
+        self.height = height
+        self.weight = weight
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(min_amount=14, max_amount=60)
+    height = IntegerRange(min_amount=120, max_amount=220)
+    weight = IntegerRange(min_amount=50, max_amount=120)
+
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        self.age = age
+        self.height = height
+        self.weight = weight
 
 
 class Slide:
-    pass
+    def __init__(
+        self,
+        name: str,
+        limitation_class: (
+            ChildrenSlideLimitationValidator,
+            AdultSlideLimitationValidator
+        )
+    ) -> None:
+        self.name = name
+        self.limitation_class = limitation_class
+
+    def can_access(self, visitor: Visitor) -> bool:
+        valid = self.limitation_class(
+            visitor.age,
+            visitor.weight,
+            visitor.height
+        )
+        print(valid.__dict__)
+        return all([
+            "_age" in valid.__dict__,
+            "_height" in valid.__dict__,
+            "_weight" in valid.__dict__
+        ])
